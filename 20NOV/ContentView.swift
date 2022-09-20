e14fb0c423eacf3a329fc4d27abf12930e9b838c
AnwarAlshammari <Anwar.oc@outlook.sa>
2022-09-20 08:48:05 +0300

    Initial Commit

---
+++ 20NOV/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  20NOV
+//
+//  Created by anwar on 24/02/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 20NOV/_0NOVApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0NOVApp.swift
+//  20NOV
+//
+//  Created by anwar on 24/02/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct _0NOVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
